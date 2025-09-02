ecbfa0382c6ec98393bac7881855df441cae8dea
Irina Makarova <iymakarova@gmail.com>
2025-09-02 21:23:33 +0200

    initial backend setup

---
+++ Backend/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var username = builder.Configuration["DB:Username"];
+var password = builder.Configuration["DB:Password"];
+var port = builder.Configuration["DB:Port"];
+var host = builder.Configuration["DB:Host"];
+var dbName = builder.Configuration["DB:Name"];
+
+var connectionString = $"postgresql://{username}:{password}@{host}:{port}/{dbName}";
+
+// builder.Services.AddDbContext<>(options => options.UseNpgsql(connectionString));
+// builder.Services.AddSingleton<UserRepository, IUserRepository>
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
